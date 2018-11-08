2fa146d3ffff46bc1f8d1de6f626f779fb56526d
Kan Lipipan <kanlip1527@gmail.com>
2018-11-08 23:47:09 +0700

    ADD analytics with verify ownershit

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import { MuiThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import store from '../store';
 import { Provider } from 'react-redux';
+import { initGA,logPageView } from '../utils/reactGa'
 import JssProvider from 'react-jss/lib/JssProvider';
 import getPageContext from '../utils/getPageContext';
 const theme = createMuiTheme({
@@ -17,14 +18,18 @@ class MyApp extends App {
   constructor(props) {
     super(props);
     this.pageContext = getPageContext();
+   
   }
   componentDidMount() {
+    initGA();
+    logPageView();
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
+  
   render() {
     const { Component, pageProps } = this.props;
     return (
@@ -32,6 +37,11 @@ class MyApp extends App {
         <Head>
           <meta charSet="utf-8" />
           <title>Kan Profile</title>
+          <meta
+            name="google-site-verification"
+            
+            content="7pNd-ymISffEbRjUbyQoxUrms4m94tJOONOnlpWhvto"
+          />
           
           <meta
             name="viewport"
