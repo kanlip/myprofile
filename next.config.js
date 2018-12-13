921391aa464b206749322f156a4c557dea4db0bb
Kan Lipipan <kanlip1527@gmail.com>
2018-12-13 22:59:10 +0700

    travis

--- next.config.js
+++ next.config.js
@@ -4,15 +4,6 @@ const withImages = require('next-images');
 module.exports = withCSS(
   withImages({
     webpack(config, options) {
-      // config.module.rules = [
-      //   {
-      //     test: /\.(pdf|gif|png|jpe?g|svg)$/,
-      //     use: [{
-      //       loader: 'file-loader',
-      //       options: {}
-      //     }]
-      //   }
-      // ]
       return config;
     }
   })
